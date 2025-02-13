3f2e2b1dc958dfb577516d5d95f70c4808bd905e
MangoFizz <ccs81.mr@gmail.com>
2025-02-13 10:34:07 +0000

    Update main.cpp and CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,22 @@
 #include <balltze/logger.hpp>
 #include <balltze/plugin.hpp>
 
-Balltze::Logger logger("Halo world");
+Balltze::Logger logger("Hellgate");
 
 BALLTZE_PLUGIN_API Balltze::PluginMetadata plugin_metadata() {
     return {
-        "Halo world",
-        "Plugin's author",
-        { 1, 0, 0, 0 },
-        { 1, 0, 0, 0 },
-        true
+        "Hellgate",
+        "MangoFizz",
+        { 1, 0, 0 },
+        { 1, 0, 0 },
+        {},
+        true,
     };
 }
 
-BALLTZE_PLUGIN_API bool plugin_init() noexcept {
-    logger.info("Hello world!");
+BALLTZE_PLUGIN_API bool plugin_load() noexcept {
+    logger.info("Loaded!");
     return true;
-}
-
-BALLTZE_PLUGIN_API void plugin_load() noexcept {
-    logger.info("Hello world again!");
 }
 
 WINAPI BOOL DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
